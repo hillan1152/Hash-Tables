d091b7a235404553ef1d17837256df6a5fb19151
Anthony Hill <hillan1152@gmail.com>
2020-05-07 16:20:03 -0400

    day 3 mvp

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -20,6 +20,8 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = [None] * self.capacity
+        self.min = 8
+        self.storage_count = 0
 
     def fnv1(self, key):
         """
@@ -60,24 +62,24 @@ class HashTable:
 
         Implement this.
         """
-        # index = random assignment of the key
-        # self.storage[index] is the value of the key
-        
-        # print("PUT INDEX --> ", index)
+        # Understand:
+            # Search list for key
+            # If key matches another, replace the value
+            # if not, append record to the list
+
         # the value being placed at the given spot
-        # self.storage[index] = value
-        # print("PUT self.storage value--> ", index)
-        # each new entry is placed within a node which will create a linked list
-        # if there is nothing inside of the index, place the new input node inside of the storage index
-        # The random place each k/v pair is placed
         index = self.hash_index(key)
+        # each new entry is placed within a node which will create a linked list
         new_input = HashTableEntry(key, value)
+        # if there's nothing there, place new input in that bucket
         if self.storage[index] is None:
             self.storage[index] = new_input
+            self.storage_count += 1
         else:
-            # will have an key, value of current node
+            # will have a key, value of current node
             curr_node = self.storage[index]
             # print("curr_node k/v ----> ", curr_node.key ,curr_node.value)
+            # while the current node exists, check to see if key matches, otherwise attach curr node to tail and place new node at the beginning
             while curr_node:
                 # does the key match?
                 if curr_node.key == key:
@@ -88,14 +90,10 @@ class HashTable:
                 tail = curr_node
                 # make our current node the next node
                 curr_node = curr_node.next
+                self.storage_count += 1
                 # print("curr node next--->", curr_node.key)
             tail.next = new_input
-                
-        # Search list for key
-        # If key matches another, replace the value
-        # if not, append record to the list
-
-
+    
 
     def delete(self, key):
         """
@@ -105,23 +103,24 @@ class HashTable:
 
         Implement this.
         """
-        warning = "Cannot Delete"
+        warning = "There's nothing to delete dude."
         index = self.hash_index(key)
-        curr_node = self.storage[index]
         # self.storage[index] brings you the value of an element at that end point
-        # if not found display warning
+        curr_node = self.storage[index]
+        # if storage bucket is empty, print warning that nothing can be deleted
         if not self.storage[index]:
             print(warning)
             return None
         # As long as self.storage exists search for the key and delete
         else: 
+            # while the node isn't empty
             while curr_node is not None: 
-            #       if self.storage exists, delete the key
                 # print("Next node", curr_node.next.value)
                 if curr_node.key == key:
                     self.storage[index] = curr_node.next
                     curr_node = None
                     print(f'{key} deleted')
+                    self.storage_count -= 1
                     return curr_node
                 elif curr_node.next is not None:
                     curr_node = curr_node.next
@@ -147,18 +146,66 @@ class HashTable:
                     return curr_node.value
                 curr_node = curr_node.next
                 # print("GET CURR NODE -->", curr_node)
-            return None
-        else:
-            return None
+        return None
         
 
     def resize(self):
         """
+        Load Factor: number of elements in list / number of slots
+        25 items in 6 slots would be a nightmare. 
+
+        If load > 0.7: double
+        If load < 0.2: halve
+            min: 8
+
+        Need to resize the hash table based on the Load Factor
+
+        Creates new table either larger or smaller based on LF.
+
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
 
         Implement this.
         """
+        # make a new, bigger table/array
+        # Go through all linked lists/indexes to count each element and create our new table 
+        # Resize is a double either larger or smaller
+        
+        # gather original storage container
+        old = self.storage
+        new_capacity = self.capacity * 2
+        print("storage count --> ", self.storage_count)
+        # contains length of old storage to determine size adjust
+        # use self.capacity as length of old storage
+        load_factor = self.storage_count / self.capacity
+        print("load factor --> ", load_factor)
+
+        if load_factor > .7:
+            self.storage = [None] * new_capacity
+        for node in old:
+            while node is not None:
+                self.put(node.key, node.value)
+                node = node.next
+
+        return
+        # iterate through the old storage to see if there are collisions
+        # for i in range(len(old)):
+        #     node = old[i]
+        #     count += 1
+        #     print("node", node.next)
+        #     print("count", count)
+        #     while node is not None:
+        #         if node.next is not None:
+        #             count += 1
+        #             self.storage[i].next = None
+ 
+
+        # count the number of nodes within the storage
+        # Get load factor count / self.capacity
+        # if lf > .7: 
+        #   new capacity * 2
+        #   rehash all key, value pairs
+        
 
 if __name__ == "__main__":
     ht = HashTable(2)
@@ -187,7 +234,7 @@ if __name__ == "__main__":
 
     # # Test resizing
     # old_capacity = len(ht.storage)
-    # ht.resize()
+    ht.resize()
     # new_capacity = len(ht.storage)
 
     # # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
