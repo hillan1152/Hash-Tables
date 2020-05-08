a13ffef8712fe457a76b229139eb68c0a0a18398
Anthony Hill <hillan1152@gmail.com>
2020-05-08 12:39:43 -0400

    maybe MVP

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,47 @@
+# Understand:
+#   Purpose:
+#       How many times does a word show up in a single string?
+#   Rules:
+#       Case = ignored
+#       Output keys = lowercase
+#       Split strings into words on any white space
+#       ignore these: " : ; , . - + = / \ | [ ] { } ( ) * ^ &
+#       If the input contains no ignored characters, return empty dictionary 
+
 def word_count(s):
-    # Implement me.
+    # convert string to lower case
+    s = s.lower()
+
+    # split defines words and eliminates white space
+    s = s.split()
+
+    # define ignored characters
+    ignored = ['"', ':', ';', ',', '.', '-', '+', '=','/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+    ignored_char = set(ignored)
+    
+    cache = dict()
+    # iterate through list
+    for word in s:
+    # check to see if ignored is in word, replace with '' if it matches
+        filtered = ''
+        for char in word:
+            if char not in ignored_char:
+                filtered = filtered + char
+        # check if word is in table, if it is add 1.
+        if filtered in cache:
+            cache[filtered] += 1
+        # if not, make it = 1
+        elif filtered != "":
+            cache[filtered] = 1
+    return cache
+    
+
+
+
+         
+            
+
 
 
 if __name__ == "__main__":
